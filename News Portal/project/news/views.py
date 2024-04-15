4efce9193f4f4a120807ec3126db7c23494eac22
ZloyKolxoznik1 <149167209+ZloyKolxoznik1@users.noreply.github.com>
2024-04-15 20:42:02 +0400

    Add files via upload

--- News Portal/project/news/views.py
+++ News Portal/project/news/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import *
+
+
+class NewsList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'
